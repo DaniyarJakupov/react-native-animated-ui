b23d9d0018c34b7c9f9d2b44cc47dbd06f7e4ee9
DaniyarJakupov <ddr199305@gmail.com>
2018-01-07 19:10:36 +0100

    Install storybook

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import StorybookUI from './storybook';
 
-AppRegistry.registerComponent('ReactNativeAnimatedUI', () => App);
+AppRegistry.registerComponent('ReactNativeAnimatedUI', () => StorybookUI);
