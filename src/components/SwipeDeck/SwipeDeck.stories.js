a2f5ffaea18acb333488ebae071ef7c418264567
DaniyarJakupov <ddr199305@gmail.com>
2018-05-09 17:03:09 +0200

    Pass data prop

--- src/components/SwipeDeck/SwipeDeck.stories.js
+++ src/components/SwipeDeck/SwipeDeck.stories.js
@@ -5,11 +5,18 @@ import { withKnobs } from '@storybook/addon-knobs';
 
 import SwipeDeck from '.';
 
+const DATA = [
+  { id: '1', uri: 'https://i.imgur.com/qFZmyFD.jpg' },
+  { id: '2', uri: 'https://i.imgur.com/sjFFcOF.jpg' },
+  { id: '3', uri: 'https://i.imgur.com/m6Z5NAw.jpg' },
+  { id: '4', uri: 'https://i.imgur.com/PY56L1n.jpg' }
+];
+
 /* SwipeDeck */
 const SwipeDeckStory = storiesOf('SwipeDeck', module);
 SwipeDeckStory.addDecorator(withKnobs);
 SwipeDeckStory.add('default', () => {
-  return <SwipeDeck />;
+  return <SwipeDeck data={DATA} />;
 });
 
 export default SwipeDeckStory;
